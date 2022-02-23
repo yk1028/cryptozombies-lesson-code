d153d6a05a75aac99ba39ead57f5a05627f93069
songwongeun <wongeunsong94@gmail.com>
2022-02-23 17:46:58 +0900

    delete random function

--- lesson-chainlink/latest/Contract.sol
+++ lesson-chainlink/latest/Contract.sol
@@ -38,11 +38,4 @@ contract ZombieFactory is VRFConsumerbase {
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness;
     }
-
-    // Delete the function below
-    function _generatePseudoRandomDna(string memory _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
-    }
-
 }
